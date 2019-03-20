c153fda9b3225c968556dcb76aefa51eba161c41
jrizza88 <jetorres88@gmail.com>
2019-03-19 22:28:51 -0400

    completed stretch goals

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ server.get('/api/cohorts/:id', async (req, res) => {
 server.get('/api/cohorts/:id/students', async (req, res) => {
     try {
         const cohort = await db('cohorts')
-        // .select('cohorts.id', 'students.name')
+        .select('students.id',  'students.name', 'cohorts.id as cohort')
         .from('cohorts')
         .innerJoin('students', 'cohorts.id', 'students.cohort_id')
         .where({cohort_id: req.params.id})
@@ -76,6 +76,68 @@ server.delete('/api/cohorts/:id', async (req, res) => {
     try {
         const cohort = await db('cohorts').where({id: req.params.id}).del()
         if(cohort > 0) {
+            res.status(204).end()
+        } else {
+            res.status(404).json({message: 'Records not found'})
+        }
+    } catch (error){
+        res.status(500).json(error)
+    }
+});
+
+
+
+server.get('/api/students', async (req, res) => {
+    try {
+        const students = await db('students')
+        res.status(200).json(students)
+    } catch (error){
+        res.status(500).json(error)
+    }
+});
+
+server.get('/api/students/:id', async (req, res) => {
+    try {
+        const students = await db('students')
+        .where({id: req.params.id})
+        .first();
+        if(!students) {
+            res.status(404).json({message: 'id not found!'})
+        }
+        res.status(200).json(students)
+    } catch (error){
+        res.status(500).json(error)
+    }
+});
+
+server.post('/api/students', async (req, res) => {
+    try{
+        const students= req.body;
+        const [id] = await db('students').insert(students)
+        const newStudent = await db('students').where({id}).first()
+        res.status(201).json(newStudent)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+server.put('/api/students/:id', async (req, res) => {
+    try{
+        const student = req.body;
+        const updateStudent= await db('students').where({id: req.params.id}).first().update(student)
+        if (!updateStudent){
+            res.status(404).json({message: 'Record not found!'})
+        }
+        res.status(200).json(updateStudent)
+    } catch (error){
+        res.status(500).json(error)
+    }
+});
+
+server.delete('/api/students/:id', async (req, res) => {
+    try {
+        const student = await db('students').where({id: req.params.id}).del()
+        if(student > 0) {
             res.status(204).end()
         } else {
             res.status(404).json({message: 'Records not found'})
